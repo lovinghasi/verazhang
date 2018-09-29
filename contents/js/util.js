7edc0ef5b23d437c4bd59c5819dacfa4d8762c17
Wei Zhang <weizhang.columbia@gmail.com>
2018-09-29 17:08:01 -0400

    Language (#19)
    Add Language choice

--- contents/js/util.js
+++ contents/js/util.js
@@ -1,9 +1,54 @@
+function changeLanguage(language) {
+   if (language == '中文') {
+      localStorage.setItem('verazhang.com?language=','中文');
+   } else {
+      localStorage.setItem('verazhang.com?language=','English');
+   }
+   location.reload();
+}
+
+function createHeaderContents(containerName, pageTitle) {
+    var buttonDiv = document.createElement('div');
+    buttonDiv.style = "padding-right: 10px;";
+    buttonDiv.align = "right";
+    // create a English button
+    var buttonEnglish = document.createElement('button');
+    buttonEnglish.type = "button";
+    buttonEnglish.style = "display: inline-block;";
+    buttonEnglish.onclick = function () {
+       changeLanguage('English'); 
+    }
+    var buttonEnglishText = document.createTextNode('English');
+    buttonEnglish.appendChild(buttonEnglishText);
+    buttonDiv.appendChild(buttonEnglish);
+    // create a Chinese button
+    var buttonChinese = document.createElement('button');
+    buttonChinese.type = "button";
+    buttonChinese.style = "display: inline-block;";
+    buttonChinese.onclick = function () {
+       changeLanguage('中文');
+    }
+    var buttonChineseText = document.createTextNode('中文');
+    buttonChinese.appendChild(buttonChineseText);
+    buttonDiv.appendChild(buttonChinese);
+    document.getElementById(containerName).appendChild(buttonDiv);
+    var title = document.createElement('h1');
+    title.align = "center";
+    var language = localStorage.getItem('verazhang.com?language=');
+    var titleText = document.createTextNode(pageTitle[language]);
+    title.appendChild(titleText);
+    document.getElementById(containerName).appendChild(title);
+}
 function createFooterLinks(containerName, links, pageName) {
     var ul = document.createElement('ul');
     ul.className = "list-inline";
+    var language = localStorage.getItem('verazhang.com?language=');
     // Featured 
     var featuredLi = document.createElement('li');
     var featuredText = document.createTextNode("Featured");
+    if (language == "中文") {
+        featuredText = document.createTextNode("主打作品");
+    }
     if (pageName == "Featured") {
         featuredLi.appendChild(featuredText);
     } else {
@@ -18,6 +63,9 @@ function createFooterLinks(containerName, links, pageName) {
     // Art 
     var artLi = document.createElement('li');
     var artText = document.createTextNode("Art works");
+    if (language == "中文") {
+        artText = document.createTextNode("艺术作品");
+    }
     if (pageName == "Art works") {
         artLi.appendChild(artText);
     } else {
@@ -31,6 +79,9 @@ function createFooterLinks(containerName, links, pageName) {
     // Blog 
     var blogLi = document.createElement('li');
     var blogText = document.createTextNode("Blog");
+    if (language == "中文") {
+        blogText = document.createTextNode("博客");
+    }
     if (pageName == "Blog") {
         blogLi.appendChild(blogText);
     } else {
@@ -44,6 +95,9 @@ function createFooterLinks(containerName, links, pageName) {
     // About me
     var aboutMeLi = document.createElement('li');
     var aboutMeText = document.createTextNode("About me");
+    if (language == "中文") {
+        aboutMeText = document.createTextNode("关于我");
+    }
     if (pageName == "About me") {
         aboutMeLi.appendChild(aboutMeText);
     } else {
@@ -78,16 +132,20 @@ function createFooterLinks(containerName, links, pageName) {
     var emailLi = document.createElement('li');
     var emailLink = document.createElement('a');
     var emailText = document.createTextNode("Email");
+    if (language == "中文") {
+        emailText = document.createTextNode("邮件");
+    }
     emailLink.appendChild(emailText);
     emailLink.className = "black-link-underline";
     emailLink.href = "mailto:wei.vera.zhang@gmail.com?Subject=From%20www.verazhang.com";
     emailLi.appendChild(emailLink);
     ul.appendChild(emailLi);
     // Support 
-
     var supportLi = document.createElement('li');
-
     var supportText = document.createTextNode("Support me");
+    if (language == "中文") {
+        supportText = document.createTextNode("支持我");
+    }
     if (pageName == "Support") {
         supportLi.appendChild(supportText);
     } else {
